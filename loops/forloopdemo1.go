5451e59c53ba3e8059e3917fa087e759935c2338
Onur Bulut <onurbulut@Onurs-MacBook-Pro.local>
2023-09-18 11:40:01 +0300

    Add loops

---
+++ loops/forloopdemo1.go
@@ -0,0 +1,11 @@
+package loops
+
+func Demo() {
+	var metin string = "Hello world"
+	i := 1
+	for i <= 5 {
+		i++
+		println(metin)
+	}
+
+}
---
+++ loops/forloopsdemo2.go
@@ -0,0 +1,8 @@
+package loops
+
+func Demo2() {
+
+	for i := 1; i <= 10; i++ {
+		println("Hello world. Sayı:", i)
+	}
+}
