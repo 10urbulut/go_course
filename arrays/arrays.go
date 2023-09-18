5c7517a7abb310fc9d0ddec0450bda2a09ed5710
Onur Bulut <onurbulut@Onurs-MacBook-Pro.local>
2023-09-18 11:39:25 +0300

    Add arrays

---
+++ arrays/arrays.go
@@ -0,0 +1,14 @@
+package arrays
+
+import "fmt"
+
+func Demo() {
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
+	}
+
+	fmt.Println(numbers)
+
+}
